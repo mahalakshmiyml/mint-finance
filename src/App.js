fe76950ad0c041d36531b7be0b37034356f89326
mahalakshmiyml <lakshmiyml@gmail.com>
2022-05-12 13:16:36 +0530

    enquiery

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import './App.scss';
-import Footer from './Components/Includes/Footer';
-import Home from './Components/Home';
-import Menubar from './Components/Includes/Menubar';
-import ScrollToTop from './Components/Includes/ScrollToTop';
+import Enquiry from './Components/Includes/new/Enquiry';
 
 function App() {
   return (
     <>
-      
-      <Menubar />
-      <ScrollToTop />
-      <Home />
-      <Footer />
+      <Enquiry />
+           
     
     </>
   );
