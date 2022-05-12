e68869a8837f24d8243cad9314832787cf89584a
Chetan Kumar N <chetankumar.nv@gmail.com>
2022-05-12 12:08:52 +0530

    a

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.scss';
 import Footer from './Components/Includes/Footer';
 import Home from './Components/Home';
 import Menubar from './Components/Includes/Menubar';
+import ScrollToTop from './Components/Includes/ScrollToTop';
 
 function App() {
   return (
     <>
       
       <Menubar />
+      <ScrollToTop />
       <Home />
       <Footer />
     
